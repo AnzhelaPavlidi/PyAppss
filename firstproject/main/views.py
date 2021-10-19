2d75ac4c45d4e9885e166e6ada399ebed2009890
ZAC <pau.mos.comm@outloo.com>
2021-10-19 17:48:50 +0300

    add class AdvertDetailView(generic.DetailView)

--- firstproject/main/views.py
+++ firstproject/main/views.py
@@ -4,6 +4,15 @@ from .models import Advert
 
 
 class AdvertListView(generic.ListView):
+    """
+    Список рекламных обьявлений
+    """
     queryset = Advert.objects.all()
     template_name = "main/advert_list.html"
+    context_object_name = 'adv'
+
+
+class AdvertDetailView(generic.DetailView):
+    model = Advert
+    template_name = "main/advertdetail.html"
     context_object_name = 'adv'
