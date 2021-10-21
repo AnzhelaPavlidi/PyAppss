38f5f7356344b6fd5fee5f668b25ae3e8bd472b9
ZAC <pau.mos.comm@outloo.com>
2021-10-21 11:42:23 +0300

    add  path('detail/<int:pk>'

--- firstproject/main/urls.py
+++ firstproject/main/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from django.http import HttpResponse
-from .views import AdvertListView
+from .views import AdvertListView, AdvertDetailView
 
 
-def testView():
+def testView(request):
+    """
+    Текстовый метод для проверки запуска тестовой страницы.
+    """
     return HttpResponse('<div align="center"><h1 style="color:red">Это тестовая страница!</h1></div>')
 
 
 urlpatterns = [
-    path('main/', AdvertListView.as_view(), name='adv_list')
+    path('', AdvertListView.as_view(), name='adv_list'),
+    path('detail/<int:pk>', AdvertDetailView.as_view(), name='adv.detail'),
+
 ]
