9b28e2b1ddca2fce98ca4a941566ad58af767637
ZAC <pau.mos.comm@outloo.com>
2021-10-21 11:58:34 +0300

    add  method get_detailUlr

--- firstproject/main/models.py
+++ firstproject/main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Advert(models.Model):
-    objects = None
-    title = models.CharField(verbose_name='заглавие', max_length=20)
+    """
+    Модель Advert, для хранения информации рекламного обьявления.
+    """
+
+    title = models.CharField(verbose_name='заглавие', max_length=50)
     text = models.TextField(verbose_name='Текст обьявления', null=True)
     phone = models.CharField(verbose_name='Телефон', max_length=15, null=True, blank=True)
     email = models.EmailField(verbose_name='Email', max_length=30, null=True)
@@ -12,3 +16,6 @@ class Advert(models.Model):
     class Meta:
         verbose_name = 'Обьявление'
         verbose_name_plural = 'Обьявления'
+
+    def get_detailUlr(self):
+        return reverse('adv_detail', kwargs={'pk': self.pk})
