11b8ae9e1030b79999ff7141d25d87ea66edf912
ZAC <pau.mos.comm@outloo.com>
2021-10-21 18:27:07 +0300

    add classes update,delete

--- firstproject/main/views.py
+++ firstproject/main/views.py
@@ -21,3 +21,17 @@ class AdvertDetailView(generic.DetailView):
 class AdvertCreate(generic.CreateView):
     form_class = AdvertForm
     template_name = 'main/advertcreate.html'
+
+
+class AdvertUpdate(generic.UpdateView):
+    model = Advert
+    form_class = AdvertForm
+    template_name = 'maim/advertupdate.html'
+    context_object_name = 'adv'
+
+
+class AdvertDelete(generic.DeleteView):
+    model = Advert
+    template_name = 'main/advert_confirm_delete.html'
+    context_object_name = 'adv'
+    success_url = '/'
