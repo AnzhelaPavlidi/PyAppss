ad3c7ce57ab47bf145469ce3b8de77da9180445e
ZAC <pau.mos.comm@outloo.com>
2021-10-24 16:04:03 +0300

    add  Photo

--- firstproject/main/admin.py
+++ firstproject/main/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Advert
+from .models import Advert, Photo
+
+''' Нужно импортировать модель,чтобы она была тут доступна'''
 
 admin.site.register(Advert)
+''' Подключаем нашу модель Advert к админке'''
+
+admin.site.register(Photo)
+''' Подключаем нашу модель Photo к админке'''
+
