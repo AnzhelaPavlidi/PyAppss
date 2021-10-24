816656d73cb039829e16247add5e235f7dcf45a6
ZAC <pau.mos.comm@outloo.com>
2021-10-24 16:38:08 +0300

    add media

--- firstproject/main/admin.py
+++ firstproject/main/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from .models import Advert, Photo
 
-''' Нужно импортировать модель,чтобы она была тут доступна'''
+" Нужно импортировать модель,чтобы она была тут доступна"
 
 admin.site.register(Advert)
-''' Подключаем нашу модель Advert к админке'''
+" Подключаем нашу модель Advert к админке"
 
 admin.site.register(Photo)
-''' Подключаем нашу модель Photo к админке'''
-
+" Подключаем нашу модель Photo к админке"
